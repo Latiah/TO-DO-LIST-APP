135a1dc66184239c1d4dae70bc2b7453d448ba1a
Latifah <kimtifah2@gmail.com>
2023-11-26 02:57:20 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+function addTask(){
+  const entered=document.getElementById("taskInput");
+  const taskList=document.getElementById("taskList");
+  if (entered.value !==""){
+const li=document.createElement("li");
+ const task=document.createTextNode(entered.value);
+    li.appendChild(task);
+    const editBtn=document.createElement("button");
+    editBtn.innerHTML="Edit";
+    editBtn.onclick=function(){
+      editTask(li);
+    }
+    li.appendChild(editBtn);
+    const deleteBtn=document.createElement("button");
+      deleteBtn.innerHTML="Delete";
+    deleteBtn.onclick=function(){
+      deleteTask(li);
+    };
+    li.appendChild(deleteBtn);
+    taskList.appendChild(li);
+    entered.value="";
+  }
+  else{
+    alert(" you have to enter a task!");
+  }
+ 
+}
+function editTask(task){
+  const text=prompt("edit this task", task.firstChild.nodeValue);
+  if(text !==null){
+    task.firstChild.nodeValue=text;
+  }
+  
+}
+taskList.addEventListener("click",function(e){
+if(e.target.tagName==="LI"){
+e.target.classList.toggle("checked");
+}},false);
+function deleteTask(task){
+  task.remove();
+
+}
+
+function saveData(){
+localStorage.setItem("data", taskList.innerHTML);
+}
+function showTask(){
+taskList.innerHTML=localStorage.getItem("data");
+}
+showTask();
